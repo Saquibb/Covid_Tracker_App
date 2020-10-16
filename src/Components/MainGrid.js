bb0e2cf9e4f0eed363cf878c25e4d9e972de56e8
saquibb <saquibmsurs@gmail.com>
2020-10-16 23:08:44 +0500

    Graph added

--- src/Components/MainGrid.js
+++ src/Components/MainGrid.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 //Import GlobalData
 import GlobalData from './GlobalData';
-
+import Chart from './Chart';
+import { fetchData } from '../Api/index';
 import CountryData from './CountryData';
 
 const useStyles = makeStyles((theme) => ({
@@ -19,6 +20,16 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function MainGrid() {
+  const [data, myData] = useState({});
+  useEffect(() => {
+    const fetchMyAPI = async () => {
+      const initialDailyData = await fetchData();
+      console.log('main grid data', initialDailyData);
+      myData(initialDailyData);
+    };
+
+    fetchMyAPI();
+  }, []);
   const classes = useStyles();
 
   return (
@@ -32,12 +43,12 @@ export default function MainGrid() {
         <Grid item xs={8}>
           <Paper className={classes.paper}>
             <Grid item xs={12}>
-              <Paper className={classes.paper}></Paper>
-            </Grid>
-            <Grid item xs={12}>
               <Paper className={classes.paper}>
                 <CountryData />
               </Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <Chart data={data} />
             </Grid>
           </Paper>
         </Grid>
